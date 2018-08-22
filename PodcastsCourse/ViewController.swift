0ec908ab4d75b385aa443a8d44ad5746c5c4afbf
Yatin Sarbalia <yatinsns@gmail.com>
2018-08-22 22:19:14 +0530

    Initial Commit

---
+++ PodcastsCourse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodcastsCourse
+//
+//  Created by Yatin Sarbalia on 8/22/18.
+//  Copyright © 2018 Yatin Sarbalia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
